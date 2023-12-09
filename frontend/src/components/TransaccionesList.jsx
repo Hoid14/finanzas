f2d63cc3c453433e6691e1d76888c1ab48b72e99
Sergio Arias <s-andres140@hotmail.com>
2023-12-09 13:02:13 -0500

    Mostrar datos del backend en el frontend

--- frontend/src/components/TransaccionesList.jsx
+++ frontend/src/components/TransaccionesList.jsx
@@ -1,18 +1,29 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import {getAllTransacciones} from '../api/transacciones.api'
 export function Transaccioneslist() {
+    const [transacciones,setTransacciones] = useState([])
 
-    {/*Este useEffect se va a ejecutar apenas cargue la pagina */}
+    /*Este useEffect se va a ejecutar apenas cargue la pagina */
     useEffect(() => {
         
         async function loadTransacciones() {
             const res = await getAllTransacciones()
-            console.log(res)
+            console.log(res.data)
+            setTransacciones(res.data)
         }
         loadTransacciones()
     }, [])
 
     return (
-        <div>TransaccionesList</div>
-    )
-}
+        <div>
+            {transacciones.map(transaccion=>(
+                <div>
+                    <h1>{transaccion.tipo}</h1>
+                    <p>{transaccion.descripcion}</p>
+                    <p>{transaccion.monto}</p>
+                </div>
+            ))}
+        </div>
+        
+        
+)}
